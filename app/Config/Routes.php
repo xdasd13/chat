57079c2043d52c6ae2eb551cf4ca5cf113019ba0
fabian yataco <yatako1808@gmail.com>
2025-10-17 12:36:35 -0500

    rutas agregadas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,12 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// Rutas para Averías
+$routes->group('averias', function($routes) {
+    $routes->get('/', 'Averias::index');
+    $routes->get('listar', 'Averias::listar');
+    $routes->get('registrar', 'Averias::registrar');
+    $routes->post('guardar', 'Averias::guardar');
+    $routes->get('actualizar/(:num)', 'Averias::actualizar/$1');
+});
